98e0904a9a729358e9f855b25e99f3dd9328b7cf
Shirajtiso <shiraj.ahmed@tisostudio.com>
2024-02-25 18:27:56 +0530

    basic understing

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h2", {id:"any"}, "hello react");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
